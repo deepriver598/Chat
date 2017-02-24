bac10b06d3aa9af2d6bf9bff3e2857947dd1dde4
deepriver598 <biz.deepriver@gmail.com>
2017-02-24 14:53:37 +0900

    add firebase config

---
+++ firebase/config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCMCahtma-tpal6kF35nI89eTKcEma2_9k",
+  authDomain: "scheduleadjuster-873a6.firebaseapp.com",
+  databaseURL: "https://scheduleadjuster-873a6.firebaseio.com",
+  storageBucket: "scheduleadjuster-873a6.appspot.com",
+  messagingSenderId: "1033033425633"
+};
